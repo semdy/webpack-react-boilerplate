7312fe33a3110c2bc82c99e636b157605657a1c3
semdy <501859275@qq.com>
2018-02-02 16:04:46 +0800

    添加server-side rendering

--- src/routes/index2.js
+++ src/routes/index2.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
-//import asyncComponent from "../components/AsyncComponent";
+import {connect} from 'react-redux'
 
 import Todo from "../containers/TodoList";
 import Test from "../containers/Test";
 import NotFound from "../containers/NotFound";
 
-export default ({props}) =>
-  <Switch>
+const ConnectedSwitch = connect(state => ({
+  location: state.location
+}))(Switch);
+
+const routeConfig = ({props}) => (
+  <ConnectedSwitch>
     <Route path="/todoList" exact component={Todo}/>
     <Route path="/test" exact component={Test}/>
     <Redirect to="/todoList"/>
     <Route component={NotFound}/>
-  </Switch>
+  </ConnectedSwitch>
+);
+
+export default connect(state => ({
+  location: state.location,
+}))(routeConfig);
