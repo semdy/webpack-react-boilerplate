c20a7d016acada0925be18f9882a90e4ee0d2703
semdy <501859275@qq.com>
2018-06-28 11:30:03 +0800

    增加语法装饰器的支持

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {addTodo} from '../actions';
 
 import styles from './addTodo.less';
 
-let AddTodo = ({dispatch}) => {
-  let input;
+@connect()
+export default class AddTodo extends Component {
 
-  return (
-    <div className={styles.todoWrapper}>
-      <form onSubmit={e => {
-        e.preventDefault();
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value));
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }}/>
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
-    </div>
-  )
+  render () {
+    let input;
+    return (
+      <div className={styles.todoWrapper}>
+        <form onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return
+          }
+          this.props.dispatch(addTodo(input.value));
+          input.value = ''
+        }}>
+          <input ref={node => {
+            input = node
+          }}/>
+          <button type="submit">
+            Add Todo
+          </button>
+        </form>
+      </div>
+    )
+  }
 };
 
-AddTodo = connect()(AddTodo);
+//AddTodo = connect()(AddTodo);
 
-export default AddTodo
+//export default AddTodo
