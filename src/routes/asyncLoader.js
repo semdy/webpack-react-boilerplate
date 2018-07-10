2cdac53b3356d1d33fd66540069624487f882330
semdy <501859275@qq.com>
2018-07-10 17:58:17 +0800

    增加自定义chunk名称

--- src/routes/asyncLoader.js
+++ src/routes/asyncLoader.js
@@ -1,6 +1,18 @@
 import asyncComponent from "../components/AsyncComponent";
 
-export const Todo = asyncComponent(() => import(/* webpackChunkName: "TodoList" */"./TodoList/TodoList"));
-export const Test = asyncComponent(() => import(/* webpackChunkName: "Test" */"./Test/Test"));
-export const Animate = asyncComponent(() => import(/* webpackChunkName: "animate" */"./animate/animate"));
-export const NotFound = asyncComponent(() => import(/* webpackChunkName: "NotFound" */"./NotFound/NotFound"));
+export const Todo = asyncComponent(() => import(
+  /* webpackChunkName: "TodoList" */
+  "./TodoList/TodoList"
+));
+export const Test = asyncComponent(() => import(
+  /* webpackChunkName: "Test" */
+  "./Test/Test"
+));
+export const Animate = asyncComponent(() => import(
+  /* webpackChunkName: "animate" */
+  "./animate/animate"
+));
+export const NotFound = asyncComponent(() => import(
+  /* webpackChunkName: "NotFound" */
+  "./NotFound/NotFound"
+));
