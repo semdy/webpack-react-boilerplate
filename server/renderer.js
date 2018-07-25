ca43c05dcd44218d3ca75985791391d0463c7a70
semdy <501859275@qq.com>
2018-07-25 17:08:40 +0800

    修改ssr模板读取时机

--- server/renderer.js
+++ server/renderer.js
@@ -11,40 +11,35 @@ const fs = require("fs");
 const isDev = process.env.NODE_ENV === 'development';
 const targetFolder = isDev ? 'public' : 'dist';
 
+const templatePath = path.resolve(__dirname, '..', targetFolder, 'index.html');
+let template = fs.readFileSync(templatePath, 'utf-8')
+
 export default (req, res, next, isNoHtml) => {
-  const filePath = path.resolve(__dirname, '..', targetFolder, 'index.html');
-  fs.readFile(filePath, 'utf8', (err, html) => {
-    if (err) {
-      console.error('err', err);
-      return res.status(404).end();
-    }
 
-    const {store, history} = configureStore({}, true);
+  const {store, history} = configureStore({}, true);
 
-    // once `store` is configured, dispatch the proper route into
-    store.dispatch(push(req.originalUrl));
+  // once `store` is configured, dispatch the proper route into
+  store.dispatch(push(req.originalUrl));
 
-    const preloadedState = serialize(store.getState());
-    const markup = isNoHtml ? '' : ReactDOMServer.renderToString(
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <App/>
-        </ConnectedRouter>
-      </Provider>
-    );
+  const preloadedState = serialize(store.getState());
+  const markup = isNoHtml ? '' : ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App/>
+      </ConnectedRouter>
+    </Provider>
+  );
 
-    html = html.replace(
-      '<div id="root"></div>',
-      `<div id="root">${markup}</div>`
-    ).replace('window.INITIAL_STATE={}', `window.INITIAL_STATE = ${preloadedState}`);
+  template = template.replace(
+    '<div id="root"></div>',
+    `<div id="root">${markup}</div>`
+  ).replace('window.INITIAL_STATE={}', `window.INITIAL_STATE = ${preloadedState}`);
 
-    if (isDev) {
-      html = html
-        .replace(/%PUBLIC_URL%/g, process.env.PUBLIC_URL)
-        .replace('</body>', '<script type="text/javascript" src="/bundle.js"></script>\n</body>');
-    }
+  if (isDev) {
+    template = template
+      .replace(/%PUBLIC_URL%/g, process.env.PUBLIC_URL)
+      .replace('</body>', '<script type="text/javascript" src="/bundle.js"></script>\n</body>');
+  }
 
-    return res.status(200).send(html);
-
-  });
+  return res.status(200).send(template);
 }
